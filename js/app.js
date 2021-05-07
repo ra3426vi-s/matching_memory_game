08ff5194cfc5afae8c6f0e7d824bd5ab72dfd12a
ra3426vi-s <49162471+ra3426vi-s@users.noreply.github.com>
2021-05-08 01:41:05 +0530

    completed matching and timer

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 /*
  * Create a list that holds all of your cards
  */
-let deck= document.querySelector(".deck");
-let restart=document.querySelector(".restart");
-let elements=document.querySelectorAll(".card");
+let deck = document.querySelector(".deck");
+let restart = document.querySelector(".restart");
+let elements = document.querySelectorAll(".card");
+let timer = document.querySelector(".timer");
 //let cardopen= document.querySelectorAll(".card.open.show");
 
-let anchorelements =document.querySelectorAll(".card i")
-allclasses=[]
-cardopen=[]
-for (let i=0;i<anchorelements.length;i++){
-allclasses.push(anchorelements[i].className);
+let anchorelements = document.querySelectorAll(".card i");
+allclasses = [];
+cardopen = [];
+var sec = 0;
+var min = 0;
+var start = 0;
+var intervalStart;
+
+for (let i = 0; i < anchorelements.length; i++) {
+  allclasses.push(anchorelements[i].className);
 }
 // let test =document.querySelectorAll(".card" ,".fa",".fa-cube")
 //console.log(allclasses)
@@ -18,11 +24,6 @@ allclasses.push(anchorelements[i].className);
 // const chinod=[];
 // const chinodclass=[];
 // let x
-
-
-
-
-
 
 /*
  * Display the cards on the page
@@ -33,116 +34,121 @@ allclasses.push(anchorelements[i].className);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length,randomIndex,temporaryValue;
-    
+  var currentIndex = array.length,
+    randomIndex,
+    temporaryValue;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        console.log(randomIndex)
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-        
-    }
-     console.log(array)
-    return array;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    console.log(randomIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  console.log(array);
+  return array;
 }
-var k=0
-var count=1
-array=[]
-deck.addEventListener('click',function (e) {
-     
-    e.target.classList.add("open","show")
-    console.log(e.target)
-    cardopen.push(e.target.querySelector(".card i"))
-    console.log(cardopen)
-    
-    if (cardopen.length%2==0){
-        if(cardopen[cardopen.length-2].className!=cardopen[cardopen.length-1].className){  
-            //console.log("not equal")      
-            setTimeout(function(){
-                // Instructions after delay
-            
-            for (var i = 0; i < elements.length; i++) {
-                //elements[i].classList.remove("card","open","show");
-                if(cardopen[cardopen.length-2]==anchorelements[i]){
-                    elements[i].classList.remove("open","show");
-                   
+var k = 0;
+var count = 1;
+array = [];
+deck.addEventListener("click", function (e) {
+  e.target.classList.add("open", "show");
+  if (start == 0) {
+    intervalStart = window.setInterval(timerStart, 1000);
+    start = start + 1;
+  }
 
-                }
-                if(cardopen[cardopen.length-1]==anchorelements[i]){
-                    elements[i].classList.remove("open","show");
-                   
+  console.log(e.target);
+  cardopen.push(e.target.querySelector(".card i"));
+  console.log(cardopen);
 
-                }
+  if (cardopen.length % 2 == 0) {
+    if (
+      cardopen[cardopen.length - 2].className !=
+      cardopen[cardopen.length - 1].className
+    ) {
+      //console.log("not equal")
+      setTimeout(function () {
+        // Instructions after delay
 
-                
-
-            } 
-            cardopen=[];  
-            },2000); 
-
+        for (var i = 0; i < elements.length; i++) {
+          //elements[i].classList.remove("card","open","show");
+          if (cardopen[cardopen.length - 2] == anchorelements[i]) {
+            elements[i].classList.remove("open", "show");
+          }
+          if (cardopen[cardopen.length - 1] == anchorelements[i]) {
+            elements[i].classList.remove("open", "show");
+          }
         }
-        if(cardopen[cardopen.length-2].className==cardopen[cardopen.length-1].className){
-            //console.log("equal")
-            cardopen=[]; 
-
-        }
+        cardopen = [];
+      }, 2000);
     }
-   
-    
-    
+    if (
+      cardopen[cardopen.length - 2].className ==
+      cardopen[cardopen.length - 1].className
+    ) {
+      //console.log("equal")
+      cardopen = [];
+    }
+  }
+});
 
-})
+restart.addEventListener("click", function (e) {
+  //var deckopen=document.getElementsByClassName("fa fa-diamond")
 
-restart.addEventListener('click',function (e) {
-   
-    //var deckopen=document.getElementsByClassName("fa fa-diamond")
-   
-    
-    //console.log(deckopen);
-    //console.log(deckopen);
-   
-    
+  //console.log(deckopen);
+  //console.log(deckopen);
 
-  
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("card","open","show");
-        elements[i].classList.add("card");
-        // chi[i]=elements[i].children;
-        // chinod[i]=elements[i].childNodes[1]
-        // console.log(chinod[i]);
-        //chinodclass[i]=elements[i].childNodes[1].classList
-       // console.log(chinodclass[i])
-        
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("card", "open", "show");
+    elements[i].classList.add("card");
+    // chi[i]=elements[i].children;
+    // chinod[i]=elements[i].childNodes[1]
+    // console.log(chinod[i]);
+    //chinodclass[i]=elements[i].childNodes[1].classList
+    // console.log(chinodclass[i])
+  }
+  //console.log(chinod);
+  var ele = shuffle(allclasses);
+  //var cardte=shuffle(elements);
+  //console.log("hi",cardte[0]);
 
-     }
-     //console.log(chinod);
-     var ele= shuffle(allclasses);
-     //var cardte=shuffle(elements);
-     //console.log("hi",cardte[0]);
-     
-     
-     for (var i = 0; i < ele.length; i++) {
-        //console.log("hey",elements[i].childNodes[1])    
-     anchorelements[i].className=ele[i]
+  for (var i = 0; i < ele.length; i++) {
+    //console.log("hey",elements[i].childNodes[1])
+    anchorelements[i].className = ele[i];
     //  console.log("hi tfis is here")
     //  console.log(elements[i].childNodes[1])
-      }
-     
-     
-    // elements[0].childNodes[0].classList.add("fa fa-paper-plane-o")
+  }
+  timerReset();
 
-     
+  // elements[0].childNodes[0].classList.add("fa fa-paper-plane-o")
+});
+function timerStart() {
+  sec = sec + 1;
+  if (sec == 60) {
+    min = min + 1;
+    sec = 0;
+  }
 
-     
-    
-  
- 
-})
-
-
+  if (sec < 10 && min < 10) {
+    timer.innerHTML = "0" + min + ":" + "0" + sec;
+  }
+  if (sec >= 10 && min < 10) {
+    timer.innerHTML = "0" + min + ":" + sec;
+  }
+  if (sec < 10 && min >= 10) {
+    s;
+    timer.innerHTML = min + ":" + "0" + sec;
+  }
+  if (sec >= 10 && min >= 10) {
+    timer.innerHTML = min + ":" + sec;
+  }
+}
+function timerReset() {
+  window.clearInterval(intervalStart);
+  timer.innerHTML = "0" + "0" + ":" + "0" + "0";
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
